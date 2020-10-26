a29aaea1602d7e5a6b2faa8fd72db19c88aa5cd0
tej3107 <54077509+tej3107@users.noreply.github.com>
2020-10-26 21:03:53 +0530

    Part 1 added

---
+++ getting_to_know_java/Getting_To_Know_Java.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package getting_to_know_java;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author SHUBHAM SINGH
+ */
+public class Getting_To_Know_Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        /*Scanner sc=new Scanner(System.in);
+        
+        int []arr = new int[5];
+        for(int i=0;i<5;++i){
+            arr[i]=sc.nextInt();
+        }*/
+        
+        for(int i=0;i<5;++i)
+            System.out.println("Hi "+"Tej");
+        String s="Tej Singh";
+        //s.hashCode();
+        System.out.println(s.chars());
+    }   
+}
